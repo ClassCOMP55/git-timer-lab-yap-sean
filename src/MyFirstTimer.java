7ec6b2a68b3315b72f9e0bafe0db71c7f48c48d5
yap-sean <s_yap1@u.pacific.edu>
2026-02-22 21:54:54 -0800

    To use a timer: 1. Create the timer object (int milliseconds, ActionListener action) 2. Setup ActionListener event handler (actionPerformed) 3. Start the timer (timer.start())

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,10 +1,15 @@
 import acm.graphics.*;
 import acm.program.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
+	private static int numTimes = 0;
 	private GLabel myLabel;
 
 	public void init() {
@@ -15,9 +20,22 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		Timer timer = new Timer(1000, this);
+		timer.setInitialDelay(3000);
+		timer.start();
+		if (numTimes == 10) timer.stop();
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("# of times called? " + numTimes);
+		myLabel.move(5,0);
+
+	}
+
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
 	}
+
 }
